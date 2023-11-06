49250101a1e5aea589553ae156b5f079f5f3a60c
EdwinSch <edwinschutjes@gmail.com>
2023-11-06 14:35:19 +0100

    add: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import illustration from "./assets/illustration-hero.svg";
+import Title from "./components/Title";
 
 function App() {
   return (
     <main>
-      <img src="" alt="" />
+      <img src={illustration} alt="illustration" className="illustration" />
+      <div className="main-content">
+        <Title text={"order summary"} />
+      </div>
     </main>
   );
 }
