869ae2f0fc6ee6c345073a6d9de0cad265235273
EdwinSch <edwinschutjes@gmail.com>
2023-11-06 16:01:18 +0100

    add: subscriptions component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import illustration from "./assets/illustration-hero.svg";
 import Title from "./components/Title";
-
+import Plans from "./components/Plans";
 import Buttons from "./components/Buttons";
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
       <img src={illustration} alt="illustration" className="illustration" />
       <div className="main-content">
         <Title text={"order summary"} />
+        <Plans />
         <Buttons />
       </div>
     </main>
---
+++ src/components/SinglePlan.jsx
@@ -0,0 +1,17 @@
+import symbol from "../assets/icon-music.svg";
+
+const SinglePlan = ({ id, title, period, price }) => {
+  return (
+    <article>
+      <img src={symbol} alt="symbol" />
+      <div className="price-wrapper">
+        <h2>{title}</h2>
+        <p className="price">
+          ${price}/{period}
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default SinglePlan;
