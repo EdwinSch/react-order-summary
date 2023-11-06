3935f4fb25b4418043dde445b1faf263d6f5f7c5
EdwinSch <edwinschutjes@gmail.com>
2023-11-06 14:53:08 +0100

    add: buttons component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import illustration from "./assets/illustration-hero.svg";
 import Title from "./components/Title";
 
+import Buttons from "./components/Buttons";
+
 function App() {
   return (
     <main>
       <img src={illustration} alt="illustration" className="illustration" />
       <div className="main-content">
         <Title text={"order summary"} />
+        <Buttons />
       </div>
     </main>
   );
