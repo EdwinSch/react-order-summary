306003567d2fd28f05b781bd00f085cff685a6ff
EdwinSch <edwinschutjes@gmail.com>
2023-11-10 11:30:29 +0100

    add: plan component + styles

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,15 +1,19 @@
 import symbol from "../assets/icon-music.svg";
 
-const SinglePlan = () => {
+const Plan = () => {
   return (
-    <article>
-      <img src={symbol} alt="symbol" />
-      <div className="price-wrapper">
-        <h2>title</h2>
-        <p className="price">$</p>
+    <article className="plan-wrapper">
+      <div className="left-wrapper">
+        <img src={symbol} alt="symbol" />
+        <div className="price-wrapper">
+          <h2>annual plan</h2>
+          <p className="price">$59.99/year</p>
+        </div>
       </div>
+
+      <button className="change-btn">Change</button>
     </article>
   );
 };
 
-export default SinglePlan;
+export default Plan;
